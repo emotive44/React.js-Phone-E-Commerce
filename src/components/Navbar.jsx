969b744077a389c2086d7b9b16f24d5918611724
marko streleshki <marko_streleshki_96@abv.bg>
2020-04-01 01:44:27 +0300

    finish with styles for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import ButtonContainer from './styles/Button';
+import NavWrapper from './styles/Navbar';
 
 class Navbar extends React.Component {
     render() {
         return(
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5 bg-primary">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5 ">
                 <Link to='/'>
                     <img src={logo} alt="store" className="navbar-brand" />
                 </Link>
                 <ul className="navbar-nav align-items-center">
+                    {/* I use a ul to be ready if we need more links in a future */}
                     <li className="nav-item ml-5">
                         <Link to='/' className="nav-link">Products</Link>
                     </li>
@@ -23,7 +25,7 @@ class Navbar extends React.Component {
                         My cart
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
---
+++ src/components/styles/Navbar.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link {
+        color: var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform: capitalize;
+    }
+`;
+
+export default NavWrapper;
